636b54ca75e16d4dd78e65b1bc1f1390958bfd4f
unknown <christelle.kandel@gmail.com>
2023-07-20 13:46:42 +0200

    crud societe mis en forme

--- src/Form/SocieteType.php
+++ src/Form/SocieteType.php
@@ -12,16 +12,66 @@ class SocieteType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('logo')
-            ->add('adresse')
-            ->add('code_postal')
-            ->add('ville')
-            ->add('tel')
-            ->add('nom_contact')
-            ->add('email')
-            ->add('website')
-            ->add('commentaires')
+            ->add('nom', null, [
+                'label' => 'Nom',
+                'attr' => array(
+                    'class' => 'form-control mb-1'
+                )                
+                ])
+            ->add('logo', null, [
+                'label' => 'logo',
+                'attr' => array(
+                    'class' => 'form-control mb-1'
+                )                
+                ])
+            ->add('adresse', null, [
+                'label' => 'Adresse',
+                'attr' => array(
+                    'class' => 'form-control mb-1'
+                )                
+                ])
+            ->add('code_postal', null, [
+                'label' => 'Code postal',
+                'attr' => array(
+                    'class' => 'form-control mb-1'
+                )                
+                ])
+            ->add('ville', null, [
+                'label' => 'Ville',
+                'attr' => array(
+                    'class' => 'form-control mb-1'
+                )                
+                ])
+            ->add('tel', null, [
+                'label' => 'Téléphone',
+                'attr' => array(
+                    'class' => 'form-control mb-1'
+                )                
+                ])
+            ->add('nom_contact', null, [
+                'label' => 'Nom du contact',
+                'attr' => array(
+                    'class' => 'form-control mb-1'
+                )                
+                ])
+            ->add('email', null, [
+                'label' => 'Email',
+                'attr' => array(
+                    'class' => 'form-control mb-1'
+                )                
+                ])
+            ->add('website', null, [
+                'label' => 'Site web',
+                'attr' => array(
+                    'class' => 'form-control mb-1'
+                )                
+                ])
+            ->add('commentaires', null, [
+                'label' => 'Commentaires à afficher pour les TC',
+                'attr' => array(
+                    'class' => 'form-control mb-1'
+                )                
+                ])
         ;
     }
 
